ecbbad8c2e4a3cb44847cf45f6b325f2e2046823
Kanapriya <kanapriya@wso2.com>
2016-02-27 14:58:14 +0530

    Tag Added

--- src/test/java/org/wso2/carbon/connector/eloqua/EloquaConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/eloqua/EloquaConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class EloquaConnectorIntegrationTest extends ConnectorIntegrationTestBase
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("eloqua-connector-1.0.0");
+        init("eloqua-connector-1.0.1-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
